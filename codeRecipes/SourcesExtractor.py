0286b6dc5ac895dd836e7d10dae7259ef33a801b
Joan Giner <giner.joan@gmail.com>
2024-10-18 19:07:16 +0200

    updates

--- codeRecipes/SourcesExtractor.py
+++ codeRecipes/SourcesExtractor.py
@@ -38,7 +38,7 @@ embeddings = HuggingFaceInstructEmbeddings(
 )
 
 # Parse PDF, extract text, and embed the text chunks
-def preprocess(data_paper, pdfPaths):
+def preprocess(data_paper):
     id = data_paper['doi'].rsplit('/', 1)[-1]
     vectorsPath = "./codeRecipes/"+data_paper['journal']+"/vectors/"+id
     print(vectorsPath+"/index.faiss")
@@ -48,6 +48,7 @@ def preprocess(data_paper, pdfPaths):
         docsearch = FAISS.load_local(vectorsPath,embeddings=embeddings,allow_dangerous_deserialization=True)
     else:
         print("Creating embeddings")
+        pdfPaths = "codeRecipes/"+data_paper['journal']+"/documents/"
         texts, sources = pr.prepare_text(file_path = pdfPaths + data_paper['doi'].rsplit('/', 1)[-1]+".pdf")
         if (texts != 'error'):
             # Create an index search    
@@ -65,34 +66,34 @@ def uses(docsearch,tags):
     uses['uses_uses_sources'] = docsearch.similarity_search_with_score("Recommended and non-recommended uses of the dataset", k=4)
 
     ## Data limits non-recommended uses
-    uses['uses_data_limits_sources'] = docsearch.similarity_search("Recommended and non-recommended uses of the dataset", k=4)
+    uses['uses_data_limits_sources'] = docsearch.similarity_search_with_score("Recommended and non-recommended uses of the dataset", k=4)
 
     ## ML approach
-    uses['uses_ml_approach_sources'] = docsearch.similarity_search("testing the data with a machine learning approach", k=4)
-    uses['uses_represents_people_sources'] = docsearch.similarity_search("The data collected represents people?", k=4)
-    uses['uses_biases_sources'] = docsearch.similarity_search("Biases issues of the data", k=4)
-    uses['uses_privacy_sources'] = docsearch.similarity_search("Privacy issues of the data", k=4)
-    uses['uses_sensitivity_sources'] = docsearch.similarity_search("sensitivity issues for specific social groups ", k=4)
+    uses['uses_ml_approach_sources'] = docsearch.similarity_search_with_score("testing the data with a machine learning approach", k=4)
+    uses['uses_represents_people_sources'] = docsearch.similarity_search_with_score("The data collected represents people?", k=4)
+    uses['uses_biases_sources'] = docsearch.similarity_search_with_score("Biases issues of the data", k=4)
+    uses['uses_privacy_sources'] = docsearch.similarity_search_with_score("Privacy issues of the data", k=4)
+    uses['uses_sensitivity_sources'] = docsearch.similarity_search_with_score("sensitivity issues for specific social groups ", k=4)
     # Maintenance policies
-    uses['uses_maintenance_policies_sources'] = docsearch.similarity_search("Maintenance policies of th dataset", k=4)
+    uses['uses_maintenance_policies_sources'] = docsearch.similarity_search_with_score("Maintenance policies of th dataset", k=4)
     return uses
 
 # Extract the collection dimensions
 def collection(docsearch, uses):    
     collection = {}
-    collection['collection_explanation_sources'] = docsearch.similarity_search("How the data have been collected", k=4)
-    collection['collection_team_sources'] = docsearch.similarity_search("The team who collects the data of the dataset", k=3)
-    collection['collection_labour_sources'] = docsearch.similarity_search("labour information about the crowdsourcing team", k=4)
+    collection['collection_explanation_sources'] = docsearch.similarity_search_with_score("How the data have been collected", k=4)
+    collection['collection_team_sources'] = docsearch.similarity_search_with_score("The team who collects the data of the dataset", k=3)
+    collection['collection_labour_sources'] = docsearch.similarity_search_with_score("labour information about the crowdsourcing team", k=4)
     # Demographic of the team
-    collection['collection_team_demographic_sources'] = docsearch.similarity_search("The collection process of the dataset", k=4)
-    collection['collection_target_demographics_sources'] = docsearch.similarity_search(" demographic data on the individuals from whom the data is collected", k=4)
+    collection['collection_team_demographic_sources'] = docsearch.similarity_search_with_score("The collection process of the dataset", k=4)
+    collection['collection_target_demographics_sources'] = docsearch.similarity_search_with_score(" demographic data on the individuals from whom the data is collected", k=4)
        
     # Does the data represent language?
-    collection['collection_speakers_demographics_sources'] = docsearch.similarity_search("Speech situation information", k=4)
+    collection['collection_speakers_demographics_sources'] = docsearch.similarity_search_with_score("Speech situation information", k=4)
     # Sources
-    collection['collection_sources_sources'] = docsearch.similarity_search("The sources where the data is collected", k=4)
+    collection['collection_sources_sources'] = docsearch.similarity_search_with_score("The sources where the data is collected", k=4)
     # Infrastructure
-    collection['collection_infrastructure_sources'] = docsearch.similarity_search("The infrastructure used to collect the data", k=4)
+    collection['collection_infrastructure_sources'] = docsearch.similarity_search_with_score("The infrastructure used to collect the data", k=4)
  
 
     return collection
@@ -103,10 +104,10 @@ def annotation(docsearch):
     annotation = {}
     ## Annotation Process
 
-    annotation["annotation_explanation_sources"] = docsearch.similarity_search("The annotation of the data", k=4)
-    annotation['annotation_team_demographi_sources'] = docsearch.similarity_search("The team who annotated the data", k=4)
-    annotation['annotation_infrastructure_sources'] = docsearch.similarity_search("tools or infrastructure to annotate the data", k=4)
-    annotation['annotation_validation_methods_sources'] = docsearch.similarity_search("Validation methods applied to the labels", k=4)
+    annotation["annotation_explanation_sources"] = docsearch.similarity_search_with_score("The annotation of the data", k=4)
+    annotation['annotation_team_demographi_sources'] = docsearch.similarity_search_with_score("The team who annotated the data", k=4)
+    annotation['annotation_infrastructure_sources'] = docsearch.similarity_search_with_score("tools or infrastructure to annotate the data", k=4)
+    annotation['annotation_validation_methods_sources'] = docsearch.similarity_search_with_score("Validation methods applied to the labels", k=4)
 
     return annotation
 
@@ -116,10 +117,10 @@ def extractor(dataPaperList):
     results = []
     for index, data_paper in dataPaperList.iterrows():
         print("Processing the: "+str(index))
-        if index > -1:
+        if index > -1 and data_paper["error"] == True:
             tags = ""
             try:
-                docsearch, finaltext = preprocess(data_paper, pdfPaths)
+                docsearch, finaltext = preprocess(data_paper)
             except:
                 docsearch = 'error'
             if (docsearch != 'error'):
@@ -140,9 +141,10 @@ def extractor(dataPaperList):
 ####################################
 # MAIN SCRIPT
 ####################################
-df = pd.read_excel("results/FullStudyAnalysis.xlsx", sheet_name="Raw Data")
+#df = pd.read_excel("results/sources.xlsx", sheet_name="Raw Data")
+df = pd.read_excel("results/sources.xlsx")
 output = "codeRecipes/sources.xlsx"
-pdfPaths = "codeRecipes/SData/manuscripts/"
+
 
 sources = extractor(df)
-sources.to_excel("sources.xlsx")
+sources.to_excel("sourcesClean.xlsx")
